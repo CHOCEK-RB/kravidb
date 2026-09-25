84c85828dd3908fee1d40c09a19eb81f1c2aede1
CHOCEK-RB <choceksalas@gmail.com>
2026-09-25 11:59:28 -0500

    chore(quality): configure clang-tidy, address sanitizer, and lsp support

--- xmake.lua
+++ xmake.lua
@@ -2,21 +2,24 @@ set_project("kravidb")
 set_version("0.1.0")
 
 add_rules("mode.debug", "mode.release")
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "." })
 
 target("kravidb")
-    set_kind("binary")
-    set_languages("c++23")
-    set_warnings("all", "error")
+set_kind("binary")
+set_languages("c++23")
+set_warnings("all", "error")
 
-    add_files("src/**.cpp")
-    add_files("src/**.cppm")
+add_files("src/**.cpp")
+add_files("src/**.cppm")
 
-    add_includedirs("src")
+add_includedirs("src")
 
-    if is_mode("debug") then
-        set_symbols("debug")
-        set_optimize("none")
-    elseif is_mode("release") then
-        set_symbols("hidden")
-        set_optimize("fastest")
-    end
+if is_mode("debug") then
+	set_symbols("debug")
+	set_optimize("none")
+	set_policy("build.sanitizer.address", true)
+	set_policy("build.sanitizer.undefined", true)
+elseif is_mode("release") then
+	set_symbols("hidden")
+	set_optimize("fastest")
+end
